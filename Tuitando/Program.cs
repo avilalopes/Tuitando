6abee9efd2586fbd7fed71b4f0fb1659ceac9ad9
avilalopes <82402280+avilalopes@users.noreply.github.com>
2021-07-23 15:33:51 -0300

    Add files via upload

---
+++ Tuitando/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; 
+
+class minhaClasse {
+
+    static void Main(string[] args) { 
+
+        Console.WriteLine("Escreva a linha de texto:");
+        string v = Console.ReadLine();
+            char[] arr = v.ToCharArray();
+            if(arr.Length <=  140) //complete a condicional
+                Console.WriteLine("TWEET");
+            else
+                Console.WriteLine("MUTE");
+    }
+
+}
